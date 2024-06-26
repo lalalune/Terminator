2415d5268fd70d027f1db28253d8333d4d2277eb
HyperEvolNet <134591523+hyperevolnet@users.noreply.github.com>
2024-06-27 02:07:25 +0800

    Create hyperzzw.py

---
+++ models/modules/hyperzzw.py
@@ -0,0 +1,41 @@
+import torch
+
+from models.modules import LocalConv
+
+
+def HyperZZW_G(self, global_hk, x):
+    """
+    Global hyperzzw by dot product
+    """
+    global_ctx_hk = x.mul(global_hk)
+        
+    global_feat = torch.matmul(global_ctx_hk, x)
+        
+    return global_feat
+
+
+class HyperZZW_L(torch.nn.Module):
+    """
+    Local hyperzzw by sliding window-based convolution
+    """
+    def __init__(
+        self,
+        SlowConvType: torch.nn.Module,
+        in_channels: int,
+        kernel_size: int,
+    ):
+        super().__init__(
+        )
+        
+        self.slow_net = SlowConvType(in_channels=in_channels, kernel_size=kernel_size)
+        
+        self.local_conv = LocalConv(in_channels=in_channels, kernel_size=kernel_size)
+        self.bias = torch.nn.Parameter(torch.zeros(1))
+    
+    def forward(self, x1, x2):
+        
+        local_ctx_hk = self.slow_net(x1)
+        
+        local_feat = self.local_conv(local_ctx_hk, x2) + self.bias
+        
+        return local_feat
