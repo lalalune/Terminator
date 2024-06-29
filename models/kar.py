66179d47d38834ccb1997f2ecb615380c8d8651f
hyperzzw <134591523+hyperevolnet@users.noreply.github.com>
2024-06-29 15:43:47 +0800

    Update kar.py

--- models/kar.py
+++ models/kar.py
@@ -69,3 +69,66 @@ class Kar3(Optimizer):
         exp_avg_sq_2.mul_(beta2).add_(grad, alpha=1 - beta2)
 
     return loss
+
+
+class Kar2(Optimizer):
+  r"""Implements Kar2 algorithm."""
+
+  def __init__(self, params, lr=1e-4, betas=(0.95)):
+    """Initialize the hyperparameters.
+    Args:
+      params (iterable): iterable of parameters to optimize or dicts defining
+        parameter groups
+      lr (float, optional): learning rate (default: 1e-4)
+      betas (Tuple[float, float], optional): coefficients used for computing
+        running averages of gradient and its square (default: (0.5, 0.95))
+    """
+
+    if not 0.0 <= lr:
+      raise ValueError('Invalid learning rate: {}'.format(lr))
+    if not 0.0 <= betas[0] < 1.0:
+      raise ValueError('Invalid beta parameter at index 0: {}'.format(betas[0]))
+    
+    defaults = dict(lr=lr, betas=betas)
+
+    super().__init__(params, defaults)
+
+  @torch.no_grad()
+  def step(self, closure=None):
+    """Performs **two** optimization steps.
+    Args:
+      closure (callable, optional): A closure that reevaluates the model
+        and returns the loss.
+    Returns:
+      the loss.
+    """
+    loss = None
+    if closure is not None:
+      with torch.enable_grad():
+        loss = closure()
+    
+    for group in self.param_groups:
+      for p in group['params']:
+        if p.grad is None:
+          continue
+
+        grad = p.grad
+        state = self.state[p]
+        
+        # State initialization
+        if len(state) == 0:
+            # Exponential moving average of gradient values
+            state['exp_avg_sq'] = torch.zeros_like(p)
+
+        exp_avg_sq = state['exp_avg_sq']
+        beta = group['betas']
+
+        sign_grad = torch.sign(grad)
+        sign_avg_sq = torch.sign(exp_avg_sq)
+        
+        p.add_(sign_grad, alpha=-group['lr'])
+        p.add_(sign_avg_sq, alpha=-group['lr'])
+        
+        exp_avg_sq.mul_(beta).add_(grad, alpha=1 - beta)
+
+    return loss
