462330e49f4a0ecf77adbb6e2be6995772e362a1
hyperzzw <134591523+hyperevolnet@users.noreply.github.com>
2024-06-29 15:51:52 +0800

    Update __init__.py

--- models/modules/__init__.py
+++ models/modules/__init__.py
@@ -1,2 +1,9 @@
 from .linear import Linear1d, Linear2d
+from .g_ibs import G_IBS
+from .hyperzzw import HyperZZW_G, HyperZZW_L, HyperZZW_2E
+from .hyper_interact import HyperChannelInteract, HyperInteract
 from .local_conv import LocalConv
+from .rgu import RGU
+from .slow_net import SlowNetBase, SlowNet_G, SlowNet_GC, SlowNet_L
+from .magnet import MAGNet, MAGNet_GC, MAGNet_L
+from .sfne_block import SFNElock, MultiBranchLayer
