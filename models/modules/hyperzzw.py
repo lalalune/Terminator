69900479749fb256e389f7500929ca3a85cde3d0
hyperzzw <134591523+hyperevolnet@users.noreply.github.com>
2024-06-30 15:12:41 +0800

    Update hyperzzw.py

--- models/modules/hyperzzw.py
+++ models/modules/hyperzzw.py
@@ -7,9 +7,9 @@ def HyperZZW_G(global_hk, x):
     """
     Global hyperzzw by dot product
     """
-    global_ctx_hk = x.mul(global_hk)
+    # global_ctx_hk = x.mul(global_hk)
         
-    global_feat = torch.matmul(global_ctx_hk, x)
+    global_feat = torch.matmul(global_hk, x)
         
     return global_feat
 
@@ -27,25 +27,22 @@ class HyperZZW_L(torch.nn.Module):
         super().__init__(
         )
         
-        self.slow_net = SlowNetType_L(in_channels=in_channels, kernel_size=kernel_size)
-        
+        self.slow_net = SlowNetType_L(in_channels=in_channels)
         self.local_conv = LocalConv(in_channels=in_channels, kernel_size=kernel_size)
-        self.bias = torch.nn.Parameter(torch.zeros(1))
+        self.fast_bias = torch.nn.Parameter(torch.zeros(1))
     
     def forward(self, x1, x2):
         local_ctx_hk = self.slow_net(x1)
-        
-        local_feat = self.local_conv(local_ctx_hk, x2) + self.bias
+        local_feat = self.local_conv(local_ctx_hk, x2) + self.fast_bias
         
         return local_feat
-
-
+    
+    
 def HyperZZW_2E(hk, x):
     """
     Hyperzzw with two elementwise multiplication
     """
     ctx_hk = x.mul(hk)
-        
     feat = torch.mul(ctx_hk, x)
-        
+    
     return feat
