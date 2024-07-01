2a6f73b57a51f4be2ac0207652976df85aae852f
hyperzzw <134591523+hyperevolnet@users.noreply.github.com>
2024-07-01 15:41:43 +0800

    Update kar.py

--- models/kar.py
+++ models/kar.py
@@ -18,11 +18,11 @@ of each optimizer:
 Both optimizers utilize sign-based updates, focusing on the directionality of the gradient rather than 
 its magnitude. This approach differs from typical gradient descent algorithms, which adjust parameters 
 based on the magnitude of the gradient. 
-The differences from the previous Sign-SGD include two points: first, we remove weight decay from the optimizer; 
-second, the gradient is no longer updated by addition with the momentum term.
+The differences from the previous Sign-SGD include two points: first, we remove weight decay from the 
+optimizer; second, the gradient is no longer updated by addition with the momentum term.
 
-The Kar optimizers also aims to provide a method that can perform multiple gradient descents in a single backpropagation 
-to perform parameter updates, thereby improving the training efficiency of the model.
+The Kar optimizers also aims to provide a method that can perform multiple gradient descents in a single 
+backpropagation to perform parameter updates, thereby improving the training efficiency of the model.
 """
 
 import torch
